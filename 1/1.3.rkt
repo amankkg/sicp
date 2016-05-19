a93f4f7f3431c4bbe91d15014ea6526e5bbc3a67
code4aman <amankubanychbek@gmail.com>
2016-05-19 23:42:30 +0600

    add ex. 1.3

---
+++ 1/1.3.rkt
@@ -0,0 +1,12 @@
+(define (f a b c)
+  (if (> a b)
+      (+ (* a a) (if (> b c) (* b b) (* c c)))
+      (+ (* b b) (if (> a c) (* a a) (* c c)))))
+
+(if (not (= (f 1 2 3) 13)) "ARGS: 1 2 3")
+(if (not (= (f 3 2 1) 13)) "ARGS: 3 2 1")
+(if (not (= (f 1 3 2) 13)) "ARGS: 1 3 2")
+(if (not (= (f 2 3 1) 13)) "ARGS: 2 3 1")
+(if (not (= (f 1 3 1) 10)) "ARGS: 1 3 1")
+(if (not (= (f 3 1 1) 10)) "ARGS: 3 1 1")
+(if (not (= (f 3 3 3) 18)) "ARGS: 3 3 3")
